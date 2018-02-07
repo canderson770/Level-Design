18345ed2b0756e198503255ccdd72f8143a2d2a6
Unknown <rp17000@gmail.com>
2018-02-07 15:54:38 -0700

    new background

---
+++ Portals/Assets/Moon.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moon : MonoBehaviour
+{
+    public float speed = .1f;
+
+    void Start()
+    {
+        StartCoroutine(Move());
+    }
+	
+    IEnumerator Move()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(.1f);
+            transform.Translate(Vector3.up * speed);
+        }
+    }
+}
